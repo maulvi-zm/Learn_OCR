4482d14d3da777e7e73752164b6a7b28ac929a5a
Maulvi Ziadinda Maulana <13522122@std.stei.itb.ac.id>
2024-01-16 16:32:09 +0700

    Update: Box and data.yaml

--- src/main.py
+++ src/main.py
@@ -1,3 +1,39 @@
 from ultralytics import YOLO
+import cv2
 
-model = YOLO("yolov8m.pt")  # initialize
+def textDetection(path):
+    model = YOLO("best.pt")
+    results = model.predict(path)
+    result = results[0]
+    output = []
+    for box in result.boxes:
+        x1, y1, x2, y2 = [
+          round(x) for x in box.xyxy[0].tolist()
+        ]
+        class_id = box.cls[0].item()
+        prob = round(box.conf[0].item(), 2)
+        output.append([
+          x1, y1, x2, y2, result.names[class_id], prob
+        ])
+    return output
+
+def drawBoundingBoxes(image_path, output):
+    # Load the image
+    image = cv2.imread(image_path)
+
+    # Draw bounding boxes on the image
+    for box in output:
+        x1, y1, x2, y2, label, prob = box
+        cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255, 0), 2)
+        label_text = f"{label}: {prob}"
+        cv2.putText(image, label_text, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+    # Display the result
+    cv2.imshow("Detected Text", image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+# Example usage:
+image_path = "images/4.jpg"
+output_from_detection = textDetection(image_path)
+drawBoundingBoxes(image_path, output_from_detection)
